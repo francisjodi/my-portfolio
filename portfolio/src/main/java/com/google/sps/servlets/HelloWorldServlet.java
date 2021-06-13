8447f84da47212d3de7f817c1f4c698b76ad4caf
jodi francis <jodiannfrancis3@gmail.com>
2021-06-13 00:00:53 +0000

    helloWorldServlet update

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,5 +1,6 @@
-package com.google.sps.servlets;
 
+package com.google.sps.servlets;
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,8 +17,26 @@ public class HelloWorldServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Jodi-Ann!</h1>");
+//   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+//     response.setContentType("text/html;");
+//     response.getWriter().println("<h1>Hello there, I am Jodi!</h1>");
+//   }
+  
+ public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    //Manually Create ArrayList String
+    String ArrayList[] = new String[] {"Hello There","see ya"};
+
+    //Convert ArrayList to JSON
+    String newJson = convertToJsonUsingGson(ArrayList);
+
+    //Send JSON to the Response
+    response.setContentType("application/json");
+    response.getWriter().println(newJson);
+  }
+
+  private String convertToJsonUsingGson(String[] arrayJsonStrings) {
+    Gson gson = new Gson();
+    String json = gson.toJson(arrayJsonStrings);
+    return json;
   }
 }
